b86c6177c3ed965dc29d9ece110079f3018f76b2
Nafis Zaman <nafis.zaman-1@ou.edu>
2015-03-23 00:38:54 -0500

    Implemented cube rotation with mouse (needs work)

--- ccc/src/Main.java
+++ ccc/src/Main.java
@@ -1,4 +1,9 @@
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 
 public class Main extends SimpleApplication {
@@ -13,6 +18,7 @@ public class Main extends SimpleApplication {
 		
 		// Disable the default controls
 		this.flyCam.setEnabled(false);
+		initInput();
 				
 		// Create default materials
 		Material blueMat = new Material(this.assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -48,5 +54,41 @@ public class Main extends SimpleApplication {
 				for(int z = 0; z < 3; ++z)
 					this.rootNode.attachChild(cube.getCubie(x, y, z).getCenterNode());
 	}
+	
+	private void initInput() {
+		this.inputManager.addMapping("Rotate_Trigger", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+		this.inputManager.addMapping("Rotate_Left", new MouseAxisTrigger(MouseInput.AXIS_X, false));
+		this.inputManager.addMapping("Rotate_Right", new MouseAxisTrigger(MouseInput.AXIS_X, true));
+		this.inputManager.addMapping("Rotate_Up", new MouseAxisTrigger(MouseInput.AXIS_Y, true));
+		this.inputManager.addMapping("Rotate_Down", new MouseAxisTrigger(MouseInput.AXIS_Y, false));
+		
+		this.inputManager.addListener(clickListener, "Rotate_Trigger");
+		this.inputManager.addListener(moveListener, "Rotate_Left", "Rotate_Right", "Rotate_Up", "Rotate_Down");
+	}
+	
+	private ActionListener clickListener = new ActionListener() {
+		public void onAction(String name, boolean keyPressed, float tpf) {
+			if(name.equals("Rotate_Trigger"))
+				isRotating = keyPressed;
+		}
+	};
+	
+	private AnalogListener moveListener = new AnalogListener() {
+		public void onAnalog(String name, float value, float tpf) {
+			if(!isRotating)
+				return;
+			
+			if(name.equals("Rotate_Left"))
+				rootNode.rotate(0.0f, value * 4, 0.0f);
+			else if(name.equals("Rotate_Right"))
+				rootNode.rotate(0.0f, -value * 4, 0.0f);
+			else if(name.equals("Rotate_Up"))
+				rootNode.rotate(value * 4, 0.0f, 0.0f);
+			else if(name.equals("Rotate_Down"))
+				rootNode.rotate(-value * 4, 0.0f, 0.0f);
+		}
+	};
+	
+	private boolean isRotating = false;
 
 }
