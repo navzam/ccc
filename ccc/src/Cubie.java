f419f5d27f3d2702cfc5c0eaa7ac901a4a0d3329
Nafis Zaman <nafis.zaman-1@ou.edu>
2015-03-31 14:24:21 -0500

    Removed old geometry moves in Cubie

--- ccc/src/Cubie.java
+++ ccc/src/Cubie.java
@@ -32,32 +32,26 @@ public class Cubie {
 		
 		geoms[Side.FRONT.value] = new Geometry("front", new Quad(cubieWidth, cubieWidth));
 		geoms[Side.FRONT.value].setLocalTranslation(-cubieWidthHalf, -cubieWidthHalf, cubieWidthHalf);
-		//geoms[Side.FRONT.value].move(0.01f, 0.01f, 0.0f);
 		
 		geoms[Side.BACK.value] = new Geometry("back", new Quad(cubieWidth, cubieWidth));
 		geoms[Side.BACK.value].setLocalTranslation(cubieWidthHalf, -cubieWidthHalf, -cubieWidthHalf);
-		//geoms[Side.BACK.value].move(0.01f, 0.01f, 0.0f);
 		geoms[Side.BACK.value].rotate(0.0f, 180 * FastMath.DEG_TO_RAD, 0.0f);
 		
 		geoms[Side.LEFT.value] = new Geometry("left", new Quad(cubieWidth, cubieWidth));
 		geoms[Side.LEFT.value].rotate(0.0f, -90 * FastMath.DEG_TO_RAD, 0.0f);
 		geoms[Side.LEFT.value].setLocalTranslation(-cubieWidthHalf, -cubieWidthHalf, -cubieWidthHalf);
-		//geoms[Side.LEFT.value].move(0.01f, 0.01f, 0.0f);
 		
 		geoms[Side.RIGHT.value] = new Geometry("right", new Quad(cubieWidth, cubieWidth));
 		geoms[Side.RIGHT.value].rotate(0.0f, 90 * FastMath.DEG_TO_RAD, 0.0f);
 		geoms[Side.RIGHT.value].setLocalTranslation(cubieWidthHalf, -cubieWidthHalf, cubieWidthHalf);
-		//geoms[Side.RIGHT.value].move(0.01f, 0.01f, 0.0f);
 		
 		geoms[Side.TOP.value] = new Geometry("top", new Quad(cubieWidth, cubieWidth));
 		geoms[Side.TOP.value].setLocalTranslation(-cubieWidthHalf, cubieWidthHalf, cubieWidthHalf);
-		//geoms[Side.TOP.value].move(0.01f, 0.01f, 0.0f);
 		geoms[Side.TOP.value].rotate(-90 * FastMath.DEG_TO_RAD, 0.0f, 0.0f);
 		
 		geoms[Side.BOTTOM.value] = new Geometry("bottom", new Quad(cubieWidth, cubieWidth));
 		geoms[Side.BOTTOM.value].rotate(90 * FastMath.DEG_TO_RAD, 0.0f, 0.0f);
 		geoms[Side.BOTTOM.value].setLocalTranslation(-cubieWidthHalf, -cubieWidthHalf, -cubieWidthHalf);
-		//geoms[Side.BOTTOM.value].move(0.01f, 0.01f, 0.0f);
 		
 		for(int i = 0; i < 6; ++i)
 			geoms[i].setMaterial(MaterialManager.getMaterial(null));
