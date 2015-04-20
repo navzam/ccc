34058f2e5c1f6baca77366e4237f654dbcab2bdd
Nafis Zaman <nafis.zaman-1@ou.edu>
2015-04-19 23:01:27 -0500

    Overlay app state calls cube app state to scramble cube

--- ccc/src/OverlayAppState.java
+++ ccc/src/OverlayAppState.java
@@ -1,4 +1,5 @@
 import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 
@@ -11,6 +12,9 @@ public class OverlayAppState extends AbstractAppState implements ScreenControlle
 	@Override
 	public void initialize(AppStateManager stateManager, Application app) {
 		super.initialize(stateManager, app);
+		
+		sApp = (SimpleApplication)app;
+		
 	}
 
 	@Override
@@ -43,8 +47,16 @@ public class OverlayAppState extends AbstractAppState implements ScreenControlle
 		
 	}
 	
-	public void scrambleCube() {
-		System.out.println("Scramble cube");
+	public void scrambleCube() {		
+		CubeAppState cubeState = sApp.getStateManager().getState(CubeAppState.class);
+		if(cubeState == null) {
+			System.err.println("Scramble failed because cube state is not attached!");
+			return;
+		}
+		
+		cubeState.scrambleCube();
 	}
+	
+	private SimpleApplication sApp;
 
 }
